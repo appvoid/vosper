f518627d31aff183a08c7d17c5213e4931f19b8b
appvoid <78444142+appvoid@users.noreply.github.com>
2023-03-21 13:26:10 -0400

    vosper 2.0
    added a new title beautiful title

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo '| | | . |_ -| . | -_|  _|
+ \_/|___|___|  _|___|_|  
+            |_|          '
 echo 'Setting up Python libraries...'
 pip3 install -r requirements.txt
 echo 'Installing ffmpeg for Whisper, type your password:'
